bbc50cf05647ec2d448e5f6692d29ddfa2ece269
Diego Gómez <diego.gomez.g@usach.cl>
2025-06-14 03:14:01 -0400

    initial commit

---
+++ utils.py
@@ -0,0 +1,8 @@
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+from skimage import data, filters, io, color, util, feature
+from scipy import ndimage as ndi
+
+print("hello from utils.py")
